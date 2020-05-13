43778870c717f85b6ba7be42557d3fd87d298be3
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-05-13 12:35:55 +0200

    Added Home page
    Dummy home page added for router demo

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return {
+    <div className="home-wrapper">
+      <img
+          className="home-image"
+          src="https://www.uncommongoods.com/images/catagory/fun-fullwidth.jpg"
+          alt="image"
+        />
+        <button className="md-button shop-button">Shop now!</button>
+        </div>
+        );
+  }
+}
+
+export default Home;
